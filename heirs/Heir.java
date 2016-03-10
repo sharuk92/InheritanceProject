5b87c3868e365b191972eb1e7f5dc8a811cfcf5b
zassyne <zassyne@gmail.com>
2016-03-10 22:04:05 +0300

    create packages to encapsulate classes

---
+++ heirs/Heir.java
@@ -0,0 +1,48 @@
+package inheritanceproject.heirs;
+
+import inheritanceproject.quotas.Quota;
+
+/**
+ *
+ * @author yassine
+ */
+public class Heir {
+    
+    protected String m_degree;
+    protected Quota m_quota;
+    protected int m_quantity;
+    public static Quota m_remaining_quota = new Quota(1, 1);
+    
+    public Heir(String degree, int quantity) {
+        m_degree = degree;
+        m_quantity = quantity;
+    }
+    
+    public void setQuota(Quota quota) {
+        m_quota = quota;
+        m_remaining_quota.decreaseBy(m_quota);
+    }
+    
+    public int getQuantity() {
+        return m_quantity;
+    }
+    
+    public String getDegree() {
+        return m_degree;
+    }
+    
+    public Quota getQuota() {
+        return m_quota;
+    }
+    public boolean exists() {
+         return m_quantity != 0;
+    }
+    
+    public static void resetRemainingQuota() {
+        m_remaining_quota = new Quota(1, 1);
+    }
+    
+    public static Quota getRemainingQuota() {
+        return m_remaining_quota;
+    }
+}
---
+++ quotas/Quota.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritanceproject.quotas;
+
+/**
+ *
+ * @author yassine
+ */
+public class Quota {
+    private int m_p;
+    private int m_q;
+    
+    public Quota(int p, int q){
+        m_p = p;
+        m_q = q;
+    }
+    public int getQ() {
+        return m_q;
+    }
+    
+    public int getP() {
+        return m_p;
+    }
+    public void setP(int p) {
+        m_p = p;
+    }
+    public void setQ(int q) {
+        m_q = q;
+    }
+    
+    public void decreaseBy(Quota quota){
+        int common_q = m_q * quota.getQ();
+        int p = m_p * quota.getQ() - m_q * quota.getP();
+        
+        this.m_p = p;
+        this.m_q = common_q;
+    }
+    
+    private int findPGCD(int p, int q) {
+        if(p % q == 0) {
+            return q;
+        }
+        else 
+            return findPGCD(q, p%q);
+    }
+    public void simplifiy() {
+        int c = findPGCD(m_p, m_q);
+        if(c != 1) {
+            m_p /= c;
+            m_q /= c;
+        }
+    }
+    @Override
+    public String toString() {
+        String str;
+        if(m_p == m_q)
+            str = String.valueOf(m_p);
+        
+        else if(m_p == 0)
+            str = String.valueOf(0);
+        else
+            str = String.valueOf(m_p) + "/" + String.valueOf(m_q);
+        
+        
+        return str;
+    }
+}
